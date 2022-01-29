9e156e2d483139b21058556888c2a87b1f94fbda
ankit <ankitchanpuria@gmail.com>
2022-01-29 16:47:22 +0000

    initial commit

---
+++ file.java
@@ -0,0 +1,9 @@
+total 3248
+-rw-rw-r-- 1 ankitchanpuriag ankitchanpuriag       5 Jan 29 16:33 check_idle
+drwxr-xr-x 2 jenkins         jenkins            4096 Jan 29 15:29 hsperfdata_jenkins
+drwxr-xr-x 2 root            root               4096 Jan 29 15:30 hsperfdata_root
+drwx------ 2 jenkins         jenkins            4096 Jan 29 15:29 jetty-0_0_0_0-8080-war-_-any-4822428663481097867
+drwx------ 2 ankitchanpuriag ankitchanpuriag    4096 Jan 29 15:31 ssh-xp2njcN1bN96
+drwx------ 3 root            root               4096 Jan 29 15:29 systemd-private-2ee5a6f32cb446d1aea566484e7abe3a-rtkit-daemon.service-EJfCZx
+drwx------ 3 root            root               4096 Jan 29 15:29 systemd-private-2ee5a6f32cb446d1aea566484e7abe3a-systemd-timesyncd.service-SR3Rod
+-rw-r--r-- 1 jenkins         jenkins         3293397 Jan 29 15:29 winstone1699812409056498672.jar
---
+++ file1.java
@@ -0,0 +1,8 @@
+check_idle
+hsperfdata_jenkins
+hsperfdata_root
+jetty-0_0_0_0-8080-war-_-any-4822428663481097867
+ssh-xp2njcN1bN96
+systemd-private-2ee5a6f32cb446d1aea566484e7abe3a-rtkit-daemon.service-EJfCZx
+systemd-private-2ee5a6f32cb446d1aea566484e7abe3a-systemd-timesyncd.service-SR3Rod
+winstone1699812409056498672.jar
